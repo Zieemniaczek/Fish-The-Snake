0345bda2e14ba073da1556b3e8979d37ea2a4b74
Zieemniaczek <jacek.kusmierczyk@wp.pl>
2018-01-26 17:10:16 +0100

    Fish The Snake v0.01
    działający model węża bez systemu kolizji

---
+++ leszcz/snakeObject.js
@@ -0,0 +1,69 @@
+function Snake() {
+  this.x = 400;
+  this.y = 400;
+  this.speed = 4;
+  this.size = 20;
+  this.xspeed = 1;
+  this.yspeed = 0;
+  this.Segments = [[this.x,this.y],[this.x-this.size,this.y]];
+
+  //dodaje segment do tablicy, dodany segment ma pozycje ostatniego dodanego segmentu([this.Segments.length-1]) - kierunek(speed) pomożony przez wielkość(size)
+  this.addSegment = function(){
+    append(
+      this.Segments,
+        [this.Segments[this.Segments.length-1][0]+(this.xspeed*this.speed)],
+        [this.Segments[this.Segments.length-1][1]+(this.yspeed*this.speed)]);
+  }
+//poruszanie pierwszego segmentu, oraz kopiowanie kolejnych segmentów od końca
+  this.update = function(){
+    this.changeDir();
+    this.Segments[0][0] += this.xspeed*this.speed;
+    this.Segments[0][1] += this.yspeed*this.speed;
+    for(let i = this.Segments.length-1;i>0;i--){
+      this.Segments[i][0] = this.Segments[i-1][0];
+      this.Segments[i][1] = this.Segments[i-1][1];
+    }
+  }
+  //rysuje wszystkie segmenty w pętli
+  this.draw = function(){
+    fill(0,0,0);
+    for(let i = 0;i<this.Segments.length;i++){
+      rect(this.Segments[i][0],this.Segments[i][1],this.size,this.size)
+    }
+  }
+  //zmienia kierunek poruszania
+  this.direction = function(x,y){
+    this.xspeed = x;
+    this.yspeed = y;
+  }
+
+
+  this.timeFromLastTurn = function(time){
+    if(millis() - time < 90){
+      return false;
+    } else {
+      return true;
+    }
+  }
+
+  //zmienia kierunek poruszania i sprawie że nie można skręcić przed upływem pewnego czasu
+  this.time = 0;
+  this.changeDir = function(){
+    if(keyIsPressed && keyCode == DOWN_ARROW && this.yspeed != -1 && this.timeFromLastTurn(this.time)){
+      this.direction(0,1);
+      this.time = millis()
+    }
+    if(keyIsPressed && keyCode == UP_ARROW && this.yspeed != 1 && this.timeFromLastTurn(this.time)){
+      this.direction(0,-1);
+      this.time = millis()
+    }
+    if(keyIsPressed && keyCode == LEFT_ARROW && this.xspeed != 1 && this.timeFromLastTurn(this.time)){
+      this.direction(-1,0);
+      this.time = millis()
+    }
+    if(keyIsPressed && keyCode == RIGHT_ARROW && this.xspeed != -1 && this.timeFromLastTurn(this.time)){
+      this.direction(1,0);
+      this.time = millis()
+    }
+  }
+}
